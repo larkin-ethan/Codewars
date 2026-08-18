8e5ae77e788b4744bba53a92ab1eecc3e2202bb3
Ethan Larkin <64321156+larkin-ethan@users.noreply.github.com>
2026-08-17 20:31:01 -0500

    Add solution - CodeHub

---
+++ 6-kyu/sum-of-digits-slash-digital-root/sum-of-digits-slash-digital-root.py
@@ -0,0 +1,31 @@
+def digital_root(n):
+    # Check if it is one digit to start with
+    # Then iterate through n getting each digit and adding it together
+    # Then check if that number is greater than 10 and if it is run it again
+    
+    # Check if length is less than 2. This will cover 0 and 1. Return the original value.
+    if n < 10:
+        return n
+    
+    # Set sum equal to the sum of the initial digits
+    sum = add_digits(n)
+    
+    # While the sum is greater than or equal to 10 or larger than 1 digit run 
+    # the add_digits again on the previous sum
+    while(sum >= 10):
+        sum = add_digits(sum)
+       
+    # Return the sum after it is less than 10
+    return sum
+​
+def add_digits(num):
+    # Reset the total every run
+    total = 0
+    
+    # Iterate through each digit in the number and add them together
+    for dig in str(num):
+        # Turn the string value back into a int then add it to the total
+        total += int(dig)
+    
+    # Return the total
+    return total
